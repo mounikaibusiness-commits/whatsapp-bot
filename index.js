67824ce534d07ee29cff82be6f8ef9ea152ef84e
mounikaibusiness-commits <mounikaibusiness@gmail.com>
2025-10-20 19:05:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,19 @@ const { Client, LocalAuth } = pkg;
 
 const client = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer: { headless: true },
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ],
+  },
 });
 
 const app = express();
